c2b5e89e20e0af3a3e01bd7f8b112818241bdb40
Markus Zisser <markus.zisser@gmx.at>
2014-03-22 17:51:35 +0100

    comments added

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -5,19 +5,19 @@ asmb:
 .LFB0:
 	.cfi_startproc
 
-	movq $0, %r8
+	movq $0, %r8 				# zaehler initialisieren
 
 .L1:
 	
-	movdqu (%rdi,%r8), %xmm0
-	movdqu (%rsi,%r8), %xmm2
+	movdqu (%rdi,%r8), %xmm0 	# die naechsten 16 bytes in xmm0 fuellen
+	movdqu (%rsi,%r8), %xmm2 	# die naechsten 16 bytes in xmm2 fuellen
 
-	pminub %xmm0, %xmm2
-	movdqu %xmm2, (%rdx,%r8)
-	addq $16, %r8
+	pminub %xmm0, %xmm2			# minimum in xmm2 schreiben
+	movdqu %xmm2, (%rdx,%r8)	# xmm2 in den hauptspeicher schreiben
+	addq $16, %r8 				# schleifenzähler um 16 erhoehen
 
-	PCMPISTRI $0, %xmm2, %xmm2
-	jnz .L1
+	PCMPISTRI $0, %xmm2, %xmm2 	# check ob ein null byte im ergebnis
+	jnz .L1 					# wenn nicht nochmal von vorne starten
 
 	rep
 	ret
